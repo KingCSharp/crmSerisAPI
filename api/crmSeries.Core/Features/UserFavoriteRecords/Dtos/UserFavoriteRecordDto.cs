7e90f8bc79718cc6119c5aa6be5485a9b69b5350
KingCSharp <intelligentdev0101@gmail.com>
2019-06-15 19:48:12 -0500

    fixed some issues with the html API docs

--- api/crmSeries.Core/Features/UserFavoriteRecords/Dtos/UserFavoriteRecordDto.cs
+++ api/crmSeries.Core/Features/UserFavoriteRecords/Dtos/UserFavoriteRecordDto.cs
@@ -6,13 +6,27 @@ namespace crmSeries.Core.Features.UserFavoriteRecords.Dtos
 {
     public class BaseUserFavoriteRecordDto
     {
+        /// <summary>
+        /// The unique identifier of the entity being favorited.
+        /// </summary>
         public int RecordId { get; set; }
+
+        /// <summary>
+        /// The type of entity being favorited. E.g., Company, Contact, Lead, etc.
+        /// </summary>
         public string RecordType { get; set; }
+
+        /// <summary>
+        /// The unique identifier of the user that is favoriting this record.
+        /// </summary>
         public int UserId { get; set; }
     }
 
     public class GetUserFavoriteRecordDto : BaseUserFavoriteRecordDto
     {
+        /// <summary>
+        /// The unique identifier of the user favorite record.
+        /// </summary>
         public int FavoriteId { get; set; }
     }
 
