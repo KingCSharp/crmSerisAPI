830da71fb0f41a3a2f91a5524443aa155607cb20
KingCSharp <intelligentdev0101@gmail.com>
2019-08-21 20:43:07 -0500

    only requests with [AcceptsApiKey] will add the parameter to swagger, added swagger oauth2 support, updated docs

--- api/crmSeries.Api/Filters/APIKeyHeaderFilter.cs
+++ api/crmSeries.Api/Filters/APIKeyHeaderFilter.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace crmSeries.Api.Filters
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class AcceptsApiKeyAttribute : Attribute
+    {
+        public bool IsRequired { get; }
+
+        public AcceptsApiKeyAttribute(bool isRequired = false)
+        {
+            IsRequired = isRequired;
+        }
+    }
+
     public class APIKeyHeaderFilter : IOperationFilter
     {
         public void Apply(Operation operation, OperationFilterContext context)
@@ -13,21 +25,33 @@ namespace crmSeries.Api.Filters
 
             operation.Parameters.Add(new NonBodyParameter
             {
-                Name = "api-key",
-                In = "header",
-                Type = "string",
-                Required = true,
-                Description = "The API key used to authenticate requests. This is provided to customers by crmSeries."
-            });
-
-            operation.Parameters.Add(new NonBodyParameter
-            {
                 Name = "email",
                 In = "header",
                 Type = "string",
                 Required = false,
                 Description = "Email address of the current user. This is used only for requests that get records for the current user. This will be replaced by identity server."
             });
+
+            var attr = context.MethodInfo?.GetCustomAttributes(typeof(AcceptsApiKeyAttribute), true);
+            if (attr == null || attr.Length == 0)
+                attr = context.MethodInfo?.DeclaringType?.GetCustomAttributes(typeof(AcceptsApiKeyAttribute), true);
+
+            if (attr != null && attr.Length > 0)
+            {
+                var isRequired = ((AcceptsApiKeyAttribute)attr[0]).IsRequired;
+
+                operation.Parameters.Add(new NonBodyParameter
+                {
+                    Name = "api-key",
+                    In = "header",
+                    Type = "string",
+                    Required = isRequired,
+                    Description = $"The API key used to authenticate requests. This is provided to customers by crmSeries.{(!isRequired ? "This is required if a valid Bearer token is not included with the request." : "")}"
+                });
+
+                if (isRequired)
+                    operation.Security = new List<IDictionary<string, IEnumerable<string>>>();
+            }
         }
     }
 }
