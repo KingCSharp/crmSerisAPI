b23b59fc511b17c37a91046ae0c1044d96e8cd37
KingCSharp <intelligentdev0101@gmail.com>
2019-08-30 23:26:20 +0000

    Merged PR 131: User search changes

--- api/crmSeries.Core/Features/Users/GetUsersRequest.cs
+++ api/crmSeries.Core/Features/Users/GetUsersRequest.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using AutoMapper.QueryableExtensions;
 using crmSeries.Core.Extensions;
 using crmSeries.Core.Domain.HeavyEquipment;
-using crmSeries.Core.Common;
 
 namespace crmSeries.Core.Features.Contacts
 {
@@ -17,29 +16,20 @@ namespace crmSeries.Core.Features.Contacts
     public class GetUsersRequest : PagedQueryRequest, IRequest<PagedQueryResult<GetUserDto>>
     {
         /// <summary>
-        /// The user's first name.  Any value entered here will return users whose
-        /// first name starts with the value entered in the input box.
+        /// Any value entered here will return users whose
+        /// first name OR last name starts with the value entered in the input box.
         /// </summary>
-        public string FirstName { get; set; }
-
-        /// <summary>
-        /// The user's last name.  Any value entered here will return users whose
-        /// last name starts with the value entered in the input box.
-        /// </summary>
-        public string LastName { get; set; }
+        public string Search { get; set; }
     }
 
     public class GetUsersRequestHandler :
         IRequestHandler<GetUsersRequest, PagedQueryResult<GetUserDto>>
     {
         private readonly HeavyEquipmentContext _context;
-        private readonly IIdentityUserContext _identity;
 
-        public GetUsersRequestHandler(HeavyEquipmentContext context,
-            IIdentityUserContext identity)
+        public GetUsersRequestHandler(HeavyEquipmentContext context)
         {
             _context = context;
-            _identity = identity;
         }
 
         public Task<Response<PagedQueryResult<GetUserDto>>> HandleAsync(GetUsersRequest request)
@@ -57,15 +47,17 @@ namespace crmSeries.Core.Features.Contacts
                  .OrderBy(x => x.UserId)
                  .AsQueryable();
 
-            if (!string.IsNullOrEmpty(request.FirstName))
-                users = users.Where(x => x.FirstName.ToLower().StartsWith(request.FirstName.ToLower()));
+            if (!string.IsNullOrEmpty(request.Search))
+            {
+                var search = request.Search.ToLower();
 
-            if (!string.IsNullOrEmpty(request.LastName))
-                users = users.Where(x => x.LastName.ToLower().StartsWith(request.LastName.ToLower()));
+                users = users.Where(x => x.FirstName.ToLower().StartsWith(search) ||
+                    x.LastName.ToLower().StartsWith(search));
+            }
 
             var count = users.Count();
 
-            result.PageCount = count / request.PageSize;
+            result.PageCount = (count + request.PageSize - 1) / request.PageSize;
             result.TotalItemCount = count;
             result.PageNumber = request.PageNumber;
             result.PageSize = request.PageSize;
