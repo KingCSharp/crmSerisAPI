0523d3358a7d183e79b9c88185d15212d9638276
KingCSharp <intelligentdev0101@gmail.com>
2019-08-31 15:32:32 -0500

    implement inspection image attachment

--- api/crmSeries.Core/Features/Inspections/AttachInspectionItemImageRequest.cs
+++ api/crmSeries.Core/Features/Inspections/AttachInspectionItemImageRequest.cs
@@ -1,42 +1,86 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using crmSeries.Core.Data;
+using crmSeries.Core.Domain.HeavyEquipment;
 using crmSeries.Core.Extensions;
 using crmSeries.Core.Features.FileStorage;
 using crmSeries.Core.Features.Inspections.Dtos;
+using crmSeries.Core.Features.Inspections.Utility;
 using crmSeries.Core.Mediator;
 using crmSeries.Core.Mediator.Attributes;
+using crmSeries.Core.Mediator.Decorators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace crmSeries.Core.Features.Inspections
 {
-    [DoNotLog, DoNotValidate] // TODO: Validator
-    public class AttachInspectionItemImageRequest : RecordAssignedInspectionItemImageDto, IRequest<GetRecordAssignedInspectionItemImageDto>
+    [DoNotLog]
+    [HeavyEquipmentContext]
+    public class AttachInspectionItemImageRequest 
+        : RecordAssignedInspectionItemImageDto, IRequest<GetRecordAssignedInspectionItemImageDto>
     {
     }
 
-    public class AttachInspectionItemImageHandler : IRequestHandler<AttachInspectionItemImageRequest, GetRecordAssignedInspectionItemImageDto>
+    public class AttachInspectionItemImageHandler 
+        : IRequestHandler<AttachInspectionItemImageRequest, GetRecordAssignedInspectionItemImageDto>
     {
+        private readonly HeavyEquipmentContext _context;
         private readonly IFileStorageProvider _storageProvider;
 
-        public AttachInspectionItemImageHandler(IFileStorageProvider storageProvider)
+        public AttachInspectionItemImageHandler(HeavyEquipmentContext context, IFileStorageProvider storageProvider)
         {
+            _context = context;
             _storageProvider = storageProvider;
         }
 
         public async Task<Response<GetRecordAssignedInspectionItemImageDto>> HandleAsync(AttachInspectionItemImageRequest request)
         {
+            if (!await _context.Set<RecordAssignedInspection>().AnyAsync(x => x.AssignedInspectionId == request.AssignedInspectionId))
+                return Error.AsResponse<GetRecordAssignedInspectionItemImageDto>(InspectionConstants.ErrorMessages.InspectionRecordNotFound);
+
+            if (!await _context.Set<RecordAssignedInspectionItem>().AnyAsync(x => x.AssignedItemId == request.AssignedItemId))
+                return Error.AsResponse<GetRecordAssignedInspectionItemImageDto>(InspectionConstants.ErrorMessages.InspectionRecordItemNotFound);
+
             var extension = Path.GetExtension(request.FileName)?.ToLower();
-            var filename = $"{Guid.NewGuid()}{extension}";
+            var filename = $"{Path.GetFileNameWithoutExtension(request.FileName)}_{Guid.NewGuid()}{extension}";
             var mimeType = extension.GetMimeType();
+            var container = $"inspectionimages-{request.AssignedInspectionId}";
+            
+            var path = await _storageProvider.StoreFileAsync(container, filename, request.FileStream, mimeType);
+            if (string.IsNullOrEmpty(path))
+                return Error.AsResponse<GetRecordAssignedInspectionItemImageDto>(InspectionConstants.ErrorMessages.ImageUploadFailed);
 
-            var path = await _storageProvider.StoreFileAsync(
-                "container", 
-                filename, 
-                request.FileStream, 
-                mimeType);
+            var entry = request.MapTo<RecordAssignedInspectionImage>();
+            entry.FileName = request.FileName;
+            entry.ImagePath = path;
 
-            //return path.MapTo<GetRecordAssignedInspectionItemImageDto>().AsResponse();
-            return new GetRecordAssignedInspectionItemImageDto().AsResponse();
+            _context.Add(entry);
+
+            await _context.SaveChangesAsync();
+
+            return entry.MapTo<GetRecordAssignedInspectionItemImageDto>().AsResponse();
+        }
+    }
+
+    public class AttachInspectionItemImageValidator : AbstractValidator<AttachInspectionItemImageRequest>
+    {
+        public AttachInspectionItemImageValidator()
+        {
+            RuleFor(x => x.AssignedInspectionId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.AssignedItemId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.FileLength)
+                .GreaterThan(0);
+
+            RuleFor(x => x.FileName)
+                .NotEmpty();
+
+            RuleFor(x => x.FileStream)
+                .NotNull();
         }
     }
 }
--- api/crmSeries.Core/Features/Inspections/Utility/InspectionsConstants.cs
+++ api/crmSeries.Core/Features/Inspections/Utility/InspectionsConstants.cs
@@ -5,8 +5,10 @@
         public static class ErrorMessages
         {
             public const string InspectionNotFound = "No inspection with this id found.";
+            public const string InspectionRecordItemNotFound = "No inspection record item with this id found.";
+            public const string InspectionRecordNotFound = "No inspection record with this id found.";
             public const string InvalidParentType = "Invalid InspectionType";
+            public const string ImageUploadFailed = "Failed to upload the image.";
         }
-
     }
 }
