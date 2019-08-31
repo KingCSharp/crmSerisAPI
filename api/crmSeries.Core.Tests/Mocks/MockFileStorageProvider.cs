62ee5508d9c5ce16adca748aa1b85d1f5d762459
KingCSharp <intelligentdev0101@gmail.com>
2019-08-31 16:34:31 -0500

    add tests

---
+++ api/crmSeries.Core.Tests/Mocks/MockFileStorageProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using crmSeries.Core.Features.FileStorage;
+
+namespace crmSeries.Core.Tests.Mocks
+{
+    public class MockFileStorageProvider : IFileStorageProvider
+    {
+        public bool GenerateMockPath { get; set; } = true;
+
+        public string Root => "Root/";
+
+        public Task<string> StoreFileAsync(string container, string fileName, Stream stream, string mimeType)
+        {
+            var path = !GenerateMockPath ? string.Empty : $"{Root}{container}/{fileName}";
+
+            return Task.FromResult(path);
+        }
+    }
+}
